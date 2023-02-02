168bd0764f4c6665dc32107cbc7a7999f8504344
Jopestpe <47086979+Jopestpe@users.noreply.github.com>
2023-02-02 17:02:03 -0300

    0.0.8 Novamente

---
+++ Scripts/Pontos.gd
@@ -0,0 +1,13 @@
+extends ColorRect
+
+func Jogar_Novamente():
+	if get_parent().has_node("Transicao"):
+		get_parent().get_node("Transicao").Animacao("Escurecer","res://Cenas/Espaco.tscn")
+	
+func _Entro(Pontos):
+	$Pontos.text = str(Pontos)
+	visible = true
+	$AnimationPlayer.play("Expandir")
+		
+func Girar():
+	$AnimationPlayer.play("Girar_Asteroide")
